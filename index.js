1747f69c217c4f3e9e1344b193d54ad8b3fda597
Ubuntu <ubuntu@ip-172-31-15-18.us-west-1.compute.internal>
2016-09-29 21:21:01 +0000

    Part 2

--- index.js
+++ index.js
@@ -32,19 +32,19 @@ http.createServer(function(req,res){
 
 var port = Number(process.env.PORT || 8080);
 var requestListener = function(req,res) {
-    fs.stat('index.html', function(error, stats) {
+    fs.stat("index.html", function(error, stats) {
 	if (error) {
 	    return console.log(error);
 	   }
-	fs.open('index.html', 'r', function(error,fd) {
+	fs.open("index.html", "r", function(error,fd) {
 	    if (error) {
 		return console.log(error);
 		}
 	    var buffersize = stats.size;
 	    var buffer = new Buffer(buffersize);
 	    fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-		var data = buffer.toString('utf8');
-		res.writeHeader(200, {'Content-Type': 'text/html' });
+		var data = buffer.toString("utf8");
+		res.writeHeader(200, {"Content-Type": "text/html" });
 		res.write(data);
 		res.end();
 		});
