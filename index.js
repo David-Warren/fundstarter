248f00e15cd111f47409d1483b97f4a84a67747a
Ubuntu <ubuntu@ip-172-31-15-18.us-west-1.compute.internal>
2016-09-30 07:41:07 +0000

    Part 1

--- index.js
+++ index.js
@@ -1,57 +1,27 @@
-var http = require('http');
+// -----Part 1a - synchronous
+
 var fs = require('fs');
+var filename = "./index.html";
 
-
-            /*-----Part 1a-------*/
-/*
-function Fundstarter(req, res) {
-    var content = fs.readFileSync('index.html');
-    res.writeHeader(200, { 'Content-Type' : 'text/html' });
-    res.write(content);
-    res.end();
-}
-
-http.createServer(Fundstarter). listen(process.env.PORT || 8080);
-*/
-
-
-             /*-----Part 1b------*/
-/*
-http.createServer(function(req,res){
-    fs.readFile('index.html',function(err, data) {
-	res.writeHead(200, { 'Content-Type' : 'text/html', 'Content-Length' : data.length});
-	res.write(data);
-	res.end();
- });
-}).listen(process.env.PORT || 8080);
-*/
-
-
-            /*------Part 2------*/
-
-
+var http = require('http');
 var port = Number(process.env.PORT || 8080);
-var requestListener = function(req,res) {
-    fs.stat("index.html", function(error, stats) {
-	if (error) {
-	    return console.log(error);
-	   }
-	fs.open("index.html", "r", function(error,fd) {
-	    if (error) {
-		return console.log(error);
-		}
-	    var buffersize = stats.size;
-	    var buffer = new Buffer(buffersize);
-	    fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-		var data = buffer.toString("utf8");
-		res.writeHeader(200, {"Content-Type": "text/html" });
-		res.write(data);
-		res.end();
-		});
-	    });
-	});
+
+/*var requestListener = function (req, res){ 
+    res.writeHead(200, {"Content-type":"text/plain"}); 
+    res.end("SIMPLE SIMPLE and fun" );
+}
+*/
+
+var buffer = fs.readFileSync(filename, "utf8");
+
+function start(resp) {
+    resp.writeHead(200, {"Content-Type": "text/html"});
+    resp.write(buf);
+    resp.end();
 };
 
-var server = http.createServer (requestListener);
-server.listen(port);
 
+var server = http.createServer(requestListener);//event emiiter 
+server.listen(port,function(){
+    console.log('Node app is running at port:',port);
+});
