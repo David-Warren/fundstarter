a24f8f1ab0c425730e7e1c2bb2d7c6e900d8c0d7
Ubuntu <ubuntu@ip-172-31-15-18.us-west-1.compute.internal>
2016-09-30 08:14:07 +0000

    Part 1.101 '

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ var port = Number(process.env.PORT || 8080);
 
 var content = fs.readFileSync(filename, "utf8");
 
-function start(resp) {
+var run = function start(resp) {
     resp.writeHead(200, {"Content-Type": "text/html"});
     resp.end(content);
 };
 
 
-var server = http.createServer(start);//event emiiter 
+var server = http.createServer(run);//event emiiter 
 server.listen(port,function(){
     console.log('Node app is running at port:',port);
 });
